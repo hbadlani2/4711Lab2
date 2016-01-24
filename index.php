7360c620303e86e18df0e092fd4e318f950a3d9a
HIMESH <HIMESH@HIMESH-TAB>
2016-01-24 15:17:17 -0800

    lab2 part2

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = '../system3';
+	$system_path = "/usr/share/pear/codeigniter/system";
 
 /*
  *---------------------------------------------------------------
@@ -113,7 +113,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+	$application_folder = "/opt/lampp/htdocs/jay-is-the-best/application";
 
 /*
  *---------------------------------------------------------------
